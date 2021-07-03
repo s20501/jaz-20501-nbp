a864b3dd993c39849c4ace2c1df3aabbebf66643
Marcin Zmuda-Trzebiatowski <s20501@pjwstk.edu.pl>
2021-07-03 17:56:18 +0200

    Log added

--- src/main/java/com/example/demo/Model/Log.java
+++ src/main/java/com/example/demo/Model/Log.java
@@ -17,4 +17,62 @@ public class Log {
     private String startDate;
     private String endDate;
     private String requestDate;
+    private Double rate;
+
+    public Log(String currency, String startDate, String endDate, String requestDate, Double rate) {
+        this.currency = currency;
+        this.startDate = startDate;
+        this.endDate = endDate;
+        this.requestDate = requestDate;
+        this.rate = rate;
+    }
+
+    public Log(Long id, String currency, String startDate, String endDate, String requestDate, Double rate) {
+        this.id = id;
+        this.currency = currency;
+        this.startDate = startDate;
+        this.endDate = endDate;
+        this.requestDate = requestDate;
+        this.rate = rate;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getCurrency() {
+        return currency;
+    }
+
+    public void setCurrency(String currency) {
+        this.currency = currency;
+    }
+
+    public String getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(String startDate) {
+        this.startDate = startDate;
+    }
+
+    public String getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(String endDate) {
+        this.endDate = endDate;
+    }
+
+    public String getRequestDate() {
+        return requestDate;
+    }
+
+    public void setRequestDate(String requestDate) {
+        this.requestDate = requestDate;
+    }
 }
--- src/main/java/com/example/demo/Service/NPBService.java
+++ src/main/java/com/example/demo/Service/NPBService.java
@@ -1,6 +1,8 @@
 package com.example.demo.Service;
 
 import com.example.demo.Model.AverageRequest;
+import com.example.demo.Model.Log;
+import com.example.demo.Repository.LogRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
@@ -9,8 +11,11 @@ public class NPBService {
 
     private RestTemplate restTemplate;
 
-    public NPBService(RestTemplate restTemplate){
+    private final LogRepository logRepository;
+
+    public NPBService(RestTemplate restTemplate,LogRepository logRepository){
         this.restTemplate = restTemplate;
+        this.logRepository = logRepository;
     }
 
     public Double getAverage(String currency, String startDate, String endDate){
@@ -18,7 +23,11 @@ public class NPBService {
 
         AverageRequest averageRequest = restTemplate.getForObject(url,AverageRequest.class);
 
-        return  averageRequest.getRates().stream().mapToDouble(r -> r.mid).average().orElse(-1.0d);
+        Double response =  averageRequest.getRates().stream().mapToDouble(r -> r.mid).average().orElse(-1.0d);
+//        Log log = new Log(currency,startDate,endDate,"2020-15-3",response);
+//        logRepository.save(log);
+
+        return response;
 
 
     }
