c7542fdd0b83eac2491a731ef354f2697a72a1b2
Marcin Zmuda-Trzebiatowski <s20501@pjwstk.edu.pl>
2021-07-03 16:53:48 +0200

    Controller

---
+++ src/main/java/com/example/demo/Controller/NBPController.java
@@ -0,0 +1,35 @@
+package com.example.demo.Controller;
+
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/nbp")
+public class NBPController {
+
+
+
+
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "OK"),
+            @ApiResponse(code = 404, message = "Not found"),
+            @ApiResponse(code = 504, message = "Internal server error")}
+    )
+    @ApiOperation(value = "Pobierz średnią walut z danego zakresu", notes = "Podaj waluty i daty")
+    @GetMapping("/{currency}/{startDate}/{endDate}")
+    public ResponseEntity<Double> getAverage(
+            @ApiParam(value = "Waluta jaką checesz uzyskać", example = "eur")
+            @PathVariable String currency,
+
+            @ApiParam(value = "Data start", example = "12-01-02")
+            @PathVariable String startDate,
+            @ApiParam(value = "Data koniec", example = "12-01-02")
+            @PathVariable String endDate
+            ){
+        return ResponseEntity.ok(0.4d);
+    }
+}
